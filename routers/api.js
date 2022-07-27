3069c1383b1d0871b79fe2ba554009508f88001a
cheonHwi <hoba0409@naver.com>
2022-07-28 00:51:37 +0900

    express-session lib added

--- routers/api.js
+++ routers/api.js
@@ -4,21 +4,9 @@ const { visitorModel } = require("../models/visitor");
 const apiRouter = express.Router();
 
 apiRouter.post("/", (req, res) => {
-  const {
-    visitorName,
-    visitorPhoneNumber,
-    visitorDivision,
-    visitorReason,
-    temp,
-  } = req.body;
+  const { visitorName, visitorPhoneNumber, visitorDivision, visitorReason, temp, ...body } = req.body;
   visitorModel
-    .create({
-      visitorName,
-      visitorPhoneNumber,
-      visitorDivision,
-      visitorReason,
-      temp,
-    })
+    .create({ visitorName, visitorPhoneNumber, visitorDivision, visitorReason, temp, ...body })
     .then((e) => {
       res.send(e);
     })
