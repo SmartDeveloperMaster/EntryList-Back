ad6db6a8ed8380616bc8eb7512aaf5d0bea4d5dc
cheonHwi <87953982+cheonHwi@users.noreply.github.com>
2022-07-29 15:15:27 +0900

    models updated

--- models/visitor.js
+++ models/visitor.js
@@ -5,9 +5,9 @@ const visitorSchema = new Schema({
     visitorPhoneNumber : {type: String, required: true},
     visitorDivision : {type: String, required: true},
     visitorReason : {type: String, required: true},
-    entranceTime : {type: Date},
+    entranceTime : {type: Date, default: Date.now()},
     exitTime : {type: Date},
-    isEntrance : {type: Boolean, default: false},
+    isEntrance : {type: Boolean, default: true},
 }, {timestamps: true});
 
 exports.visitorModel = model('visitor', visitorSchema);
