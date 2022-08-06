ab0dcca6ec0cf97259e89e780b525d2d4bb6243a
cheonHwi <hoba0409@naver.com>
2022-08-06 21:23:23 +0900

    make function (return Params)

--- controllers/visitorCtrl.js
+++ controllers/visitorCtrl.js
@@ -1,7 +1,13 @@
-const { visitorModel } = require('../models/visitorModel');
-const { QrCodeModel } = require('../models/qrCodeModel');
+const abrequire = require('abrequire');
+// const { visitorModel } = require(abrequire("models/visitor"));
+// const { QrCodeModel } = require(abrequire("models/qrCode"));
 
-const SALTING = parseInt(process.env.SALTING);
+// const SALTING = parseInt(process.env.SALTING);
 
 const sendVisitorData = (result) => {
+    console.log(result)
+    console.log(typeof result)
+    return result
 } 
+
+module.exports = { sendVisitorData };
